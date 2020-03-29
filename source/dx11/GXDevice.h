8fea2cca97848bbb2e2235cbeee14b32bef59b3f
D-AIRY <admin@ds-servers.com>
2020-03-29 18:27:17 +0300

    Updated DirectX sdk

--- source/dx11/GXDevice.h
+++ source/dx11/GXDevice.h
@@ -10,7 +10,7 @@
 #include <DxErr.h>
 #include <d3dcompiler.h>
 
-#include <Windows.h>
+// #include <Windows.h>
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
 #pragma comment(lib, "DxErr.lib")
